7c7ebaec3a7b7bf4e172a7acdec16dd82880ce61
Artur <arturunkulov30@gmail.com>
2024-12-25 18:40:35 +0600

    update my code Artur

--- repetitionPython.py
+++ repetitionPython.py
@@ -1,28 +1,2 @@
 
 #################   ПОВТОРЕНИЕ PYTHON     ##################################3
-
-#
-#
-#
-#
-# a =  int(input("Введите a"))
-# oper = input("oper")
-# b = int(input("Введите b"))
-#
-# if oper == "+":
-#     print(a+b)
-#
-# elif oper == "-":
-#     print(a-b)
-#
-
-
-#
-# i = 2
-#
-# while i <= 100:
-#     print(i)
-#     i = i + 2
-
-
-
